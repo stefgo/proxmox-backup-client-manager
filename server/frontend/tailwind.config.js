ee6066f25005784f509a6d79dd55456ef943e591
stefgo <stefan.goetzl@t-online.de>
2026-03-09 14:02:09 +0100

    fix: Configure Tailwind to scan compiled output of `@stefgo/react-ui-components` for styling.

--- server/frontend/tailwind.config.js
+++ server/frontend/tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class',
+    darkMode: "class",
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
-        "../../../react-ui-components/src/**/*.{js,ts,jsx,tsx}"
+        "../../../react-ui-components/src/**/*.{js,ts,jsx,tsx}",
+        "../../node_modules/@stefgo/react-ui-components/dist/**/*.{js,mjs}",
     ],
     theme: {
         extend: {
             colors: {
                 // Custom color palette for "Premium Design"
                 proxmox: {
-                    dark: '#1e1e1e',
-                    light: '#f5f5f5',
-                    accent: '#E57000', // Proxmox Orange-ish
-                }
-            }
+                    dark: "#1e1e1e",
+                    light: "#f5f5f5",
+                    accent: "#E57000", // Proxmox Orange-ish
+                },
+            },
         },
     },
     plugins: [],
-}
+};
